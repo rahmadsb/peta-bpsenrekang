e7311dd4a9db982174be242d5545f603b8b5016a
Rahmad Setiya Budi <222011623@stis.ac.id>
2025-07-28 20:08:36 +0800

    Refactor dashboard view to improve layout and readability. Update statistics display for data requests, sent data, processed requests, and rejected requests. Enhance form structure for new data requests. Modify header image animation effect.

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -37,5 +37,5 @@
 
     <!-- Preloader -->
     <div class="preloader flex-column justify-content-center align-items-center">
-      <img class="animation__shake" src=<?= base_url("logo_bps.svg") ?> alt="AdminLTELogo" height="60" width="60">
+      <img class="animation__wobble" src=<?= base_url("logo_bps.svg") ?> alt="AdminLTELogo" height="60" width="60">
     </div>
